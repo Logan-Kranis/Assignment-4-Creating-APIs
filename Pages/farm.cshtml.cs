a694f5143b22068f5b45090e2c0283c62e35115f
Logan-Kranis <lk00028@mix.wvu.edu>
2024-03-25 11:50:43 -0400

    api addition

---
+++ Pages/farm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webpage_assigment4.Pages
+{
+    public class farmModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
