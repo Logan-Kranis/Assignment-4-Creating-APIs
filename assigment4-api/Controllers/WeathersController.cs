1ffece5d0a116d26eed9fbbe90a4ec024ab513ed
Logan-Kranis <lk00028@mix.wvu.edu>
2024-03-27 22:45:01 -0400

    added weather controler

---
+++ assigment4-api/Controllers/WeathersController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using assigment4_api.Data;
+using assigment4_api.entities;
+
+namespace assigment4_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeathersController : ControllerBase
+    {
+        private readonly DBcontextClass _context;
+
+        public WeathersController(DBcontextClass context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Weathers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Weather>>> GetWeather()
+        {
+            return await _context.Weather.ToListAsync();
+        }
+
+        // GET: api/Weathers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Weather>> GetWeather(int id)
+        {
+            var weather = await _context.Weather.FindAsync(id);
+
+            if (weather == null)
+            {
+                return NotFound();
+            }
+
+            return weather;
+        }
+
+        // PUT: api/Weathers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWeather(int id, Weather weather)
+        {
+            if (id != weather.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(weather).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WeatherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Weathers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Weather>> PostWeather(Weather weather)
+        {
+            _context.Weather.Add(weather);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWeather", new { id = weather.Id }, weather);
+        }
+
+        // DELETE: api/Weathers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteWeather(int id)
+        {
+            var weather = await _context.Weather.FindAsync(id);
+            if (weather == null)
+            {
+                return NotFound();
+            }
+
+            _context.Weather.Remove(weather);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool WeatherExists(int id)
+        {
+            return _context.Weather.Any(e => e.Id == id);
+        }
+    }
+}
