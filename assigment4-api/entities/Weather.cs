c4ba65f4cad7cb4275a279dc2f793b0d0eda77c1
Logan-Kranis <lk00028@mix.wvu.edu>
2024-03-27 20:55:12 -0400

    added weather entity

--- assigment4-api/entities/Weather.cs
+++ assigment4-api/entities/Weather.cs
@@ -1,6 +1,18 @@
-﻿namespace assigment4_api.entities
+﻿using System.ComponentModel.DataAnnotations;
+//Logan Kranis
+namespace assigment4_api.entities
 {
+  
+
     public class Weather
     {
+        public decimal? Temperature { get; set; }
+        public decimal? Pressure { get; set; }
+        public decimal? Humidity { get; set; }
+        public decimal? Precipitation { get; set; }
+        public decimal? WindSpeed { get; set; }
+        public int Id { get; set; }
+        public DateTime? Date { get; set; }
     }
+
 }
