95e41ea00a8a4fb609fa47aeccad2b34c4514b9b
aismail10 <142919927+aismail10@users.noreply.github.com>
2024-03-28 23:55:57 -0400

    Updated API's

--- assigment4-api/Repo/FarmService.cs
+++ assigment4-api/Repo/FarmService.cs
@@ -1,5 +1,6 @@
 ﻿using assigment4_api.Data;
 using assigment4_api.entities;
+using assigment4_api.Repo;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 //Logan Kranis
@@ -9,16 +10,40 @@ namespace assigment4_api.Repo
     {
         private readonly DBcontextClass _dbcontextClass;
 
-        public FarmService(DBcontextClass dbcontextClass) 
-        { 
+        public FarmService(DBcontextClass dbcontextClass)
+        {
             _dbcontextClass = dbcontextClass;
         }
 
-        public async Task<List<Farm>> GetFarmsbyCity(int cityfarm ) 
-        { 
-            var param = new SqlParameter("@city", cityfarm );
-            var cityfdetails = await Task.Run(() => _dbcontextClass.Farm.FromSqlRaw("exec SelectFarmsByCity @city",param).ToListAsync());
+        public async Task<List<Farm>> GetFarmsbyCity(int cityfarm)
+        {
+            var param = new SqlParameter("@city", cityfarm);
+            var cityfdetails = await Task.Run(() => _dbcontextClass.Farm.FromSqlRaw("exec SelectFarmsByCity @city", param).ToListAsync());
             return cityfdetails;
         }
+        //Ahmed Ismail
+        public async Task<List<Farm>> GetUpdateFarms(int Updatefarms)
+        {
+
+            var param = new SqlParameter("@FarmID", Updatefarms);
+            var Updatefarmsdetails = await Task.Run(() => _dbcontextClass.Farm.FromSqlRaw("exec SelectUpdateFarm @FarmID ", param).ToListAsync());
+            return Updatefarmsdetails;
+
+        }
+        public async Task<List<Farm>> DeleteFarms(int DeleteFarms)
+        {
+            var param = new SqlParameter("@FarmID", DeleteFarms);
+            var Deletefarms = await Task.Run(() => _dbcontextClass.Farm.FromSqlRaw("exec SelectDeleteFarm @FarmID ", param). ToListAsync());
+            return Deletefarms;
+        }
+
     }
+
+
 }
+
+
+
+    
+
+
--- assigment4-api/Repo/InterfaceFarmService.cs
+++ assigment4-api/Repo/InterfaceFarmService.cs
@@ -1,9 +1,14 @@
 ﻿using assigment4_api.entities;
+using Microsoft.Identity.Client;
 //Logan Kranis
 namespace assigment4_api.Repo
 {
     public interface InterfaceFarmService
     {
         public Task<List<Farm>> GetFarmsbyCity(int cityfarm);
+
+        //Ahmed Ismail 
+        public Task<List<Farm>> GetUpdateFarms(int updatefarm);
+        public Task<List<Farm>> DeleteFarms(int deletefarm);
     }
 }
