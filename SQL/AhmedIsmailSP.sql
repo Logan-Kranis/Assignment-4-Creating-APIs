855ee6bdcae6a80b405be9ba573fc62a70b613e4
Logan-Kranis <142769039+Logan-Kranis@users.noreply.github.com>
2024-04-15 20:26:49 -0400

    Add files via upload

---
+++ SQL/AhmedIsmailSP.sql
@@ -0,0 +1,27 @@
+CREATE or ALTER PROCEDURE [dbo].[UpdateFarm]
+    @FARM_ID INT,
+    @address NVARCHAR(MAX),
+    @zipcode NVARCHAR(MAX),
+    @city NVARCHAR(MAX),
+    @state NVARCHAR(MAX),
+    @name NVARCHAR(MAX)
+AS
+BEGIN
+    UPDATE [dbo].[farm]
+    SET [address] = @address,
+        [zipcode] = @zipcode,
+        [city] = @city,
+        [state] = @state,
+        [name] = @name
+    WHERE [FARM_ID] = @FARM_ID;
+END;
+GO
+
+CREATE or ALTER PROCEDURE [dbo].[DeleteFarm]
+    @FARM_ID INT
+AS
+BEGIN
+    DELETE FROM [dbo].[farm]
+    WHERE [FARM_ID] = @FARM_ID;
+END;
+GO
